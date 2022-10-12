4b59922359461cac650e5241612c58d6192d50bd
swyoon <sangwoong24yoon@gmail.com>
2022-10-12 08:41:28 +0000

    add server-client example

--- src/server/main.py
+++ src/server/main.py
@@ -3,6 +3,7 @@ from typing import Union
 import numpy as np
 from fastapi import FastAPI
 from pydantic import BaseModel
+from vit_ood import get_model
 
 
 class InputData(BaseModel):
@@ -12,9 +13,13 @@ class InputData(BaseModel):
 
 app = FastAPI()
 
+model = get_model()
+
 
 @app.post("/")
 async def root(item: InputData):
     # return {"message": "Hello World"}
     batch_x = np.array(item.batch_x)
-    return {"batch_x": batch_x.tolist()}
+    pred = model.predict(batch_x)
+    # return {'batch_x': batch_x.tolist(), 'pred': pred.tolist()}
+    return {"pred": pred.tolist()}
